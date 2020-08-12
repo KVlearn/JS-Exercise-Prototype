832ed8ec47b34bf5e22e5a4042b17437d7a42278
KVlearn <kavya.mdu@gmail.com>
2020-08-11 21:06:25 -0700

    mvp and stretch complete

--- index.js
+++ index.js
@@ -39,10 +39,23 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name,age) {
+this.name=name;
+this.age=age;
+this.stomach=[];
 }
-
+Person.prototype.eat = function(somefood){
+  if (this.stomach.length < 10){
+    this.stomach.push(somefood);
+  } return this.stomach;
+}
+Person.prototype.poop = function(){
+  this.stomach=[];
+}
+Person.prototype.toString = function(){
+  return(`${this.name} , ${this.age}`);
+}
+console.log(new Person('Mary',50));
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -57,8 +70,27 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model,milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank=0;
+this.odometer=0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
+}
+Car.prototype.drive = function(distance){
+  let maxMiles= this.tank * this.milesPerGallon; /* max distance possible */
+  /*increase odometer*/
+  if(distance <= maxMiles){
+    this.tank = this.tank - distance/this.milesPerGallon;
+    this.odometer += distance;
+  }else{
+    /*distance is more than max possible miles, reset odometer and tank*/
+    this.tank =0;
+    this.odometer= maxMiles;
+    return(`I ran out of fuel at ${this.odometer} miles!`);
+  }
 }
 
 /*
@@ -68,18 +100,27 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name,age,urToy) {
+/*copy parent's property using .call */
+  Person.call(this,name,age);
+  this.favoriteToy = urToy;
 }
+/*getting child's prototype equal to parent's*/
+Baby.prototype = Object.create(Person.prototype);
 
+Baby.prototype.play = function(){
+  return (`Playing with ${this.favoriteToy}`);
+};
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global Scope : This is Window Binding - scope of 'this' when its outside
+  any specific object.
+  2. Implicit Binding - To use 'this' with a specific declared object
+  3. "New" Binding - To use 'New' with constructor to build new objects.
+  4. Explicit Binding - Using Call and Apply. Here we choose some object
+  for some function to copy parent's property.
 */
 
 
